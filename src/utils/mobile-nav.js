3513e0c74290c7247eb0c1ecf2c4d59a6c4820bd
Chonchol Kumar Ray <fusitivechonchol@gmail.com>
2023-09-27 06:57:42 +0600

    uploading all my development code

---
+++ src/utils/mobile-nav.js
@@ -0,0 +1,39 @@
+const mobileNav = () => {
+    const headerBtn = document.querySelector('.header__bars');
+    const mobileNav = document.querySelector('.mobile-nav');
+    const mobileLinks = document.querySelectorAll('.mobile-nav__link');
+    const sunBtn = document.querySelectorAll('#theme-toggle');
+
+    // State
+    let isMobileNavOpen = false;
+
+
+    headerBtn.addEventListener('click', () => {
+        isMobileNavOpen = !isMobileNavOpen;
+        if (isMobileNavOpen) {
+            mobileNav.style.display = 'flex';
+            document.body.style.overflow = 'hidden';
+        } else {
+            mobileNav.style.display = 'none';
+            document.body.style.overflow = 'auto';
+        }
+    });
+    mobileLinks.forEach(Link => {
+        Link.addEventListener('click', () => {
+            isMobileNavOpen = false;
+            mobileNav.style.display = 'none';
+            document.body.style.overflow = 'auto';
+        });
+    });
+    sunBtn.forEach(Link => {
+        Link.addEventListener('click', () => {
+            isMobileNavOpen = false;
+            mobileNav.style.display = 'none';
+            document.body.style.overflow = 'auto';
+        });
+    });
+
+
+};
+
+export default mobileNav;
